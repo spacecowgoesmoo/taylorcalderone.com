8f2d2e73618dc9048df009bbc731c77c385eb1f9
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-23 12:02:12 -0700

    HTTPS links

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ My name is Taylor Calderone and I'm a Game Developer / Music Producer.
 I was part of <a href='http://www.bovinedragonsoftware.com' target='blank'>Bovinedragon Software</a> 
 during the early iOS era and we created
 <a href ="https://web.archive.org/web/20190211195412/http://www.slidetoplay.com/gomi-review/" target='_blank' rel="noopener">three</a>
-<a href ="http://toucharcade.com/2013/09/30/trace-2-review-years-ahead-of-the-first-game/" target='_blank' rel="noopener">awesome</a> iOS games.
+<a href ="https://toucharcade.com/2013/09/30/trace-2-review-years-ahead-of-the-first-game/" target='_blank' rel="noopener">awesome</a> iOS games.
 I mostly make web browser games now.
 
 <br><br><br><br>email - spacecowgoesmoo@gmail.com
-<br>twitter - <a href="http://twitter.com/spacecowgoesmoo" target='_blank' rel="noopener">@spacecowgoesmoo</a>
+<br>twitter - <a href="https://twitter.com/spacecowgoesmoo" target='_blank' rel="noopener">@spacecowgoesmoo</a>
 </div>
