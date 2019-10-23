5280545b04badf2ed88867404a9e2436e2b040ab
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-23 11:36:54 -0700

    Bugfixes for Google Lighthouse

--- games.php
+++ games.php
@@ -11,32 +11,32 @@
 
 
 	<div class="gridItem">
-		<a href="https://www.kongregate.com/games/spacecowgoesmoo/transcend" target="_blank">
-		<img class='gridImageKongRectangle' src="Images/transcendGameIcon.png"></a>
+		<a href="https://www.kongregate.com/games/spacecowgoesmoo/transcend" target="_blank" rel="noopener">
+		<img class='gridImageKongRectangle' src="Images/transcendGameIcon.png" alt='Transcend'></a>
 		<span class='gridText'>Transcend</span>
 	</div>	
 
 	<div class="gridItem">	
-		<a href="https://www.kongregate.com/games/spacecowgoesmoo/minites" target="_blank">
-		<img class='gridImageKongRectangle' src="Images/minitesGameIcon.png"></a>
+		<a href="https://www.kongregate.com/games/spacecowgoesmoo/minites" target="_blank" rel="noopener">
+		<img class='gridImageKongRectangle' src="Images/minitesGameIcon.png" alt='MiniTES'></a>
 		<span class='gridText'>MiniTES</span>
 	</div>
 
 	<div class="gridItem">	
-		<a href="https://itunes.apple.com/us/app/trace-2/id335668591" target="_blank">
-		<img class='gridImage' src="Images/trace2GameIcon.png"></a>
+		<a href="https://itunes.apple.com/us/app/trace-2/id335668591" target="_blank" rel="noopener">
+		<img class='gridImage' src="Images/trace2GameIcon.png" alt='Trace 2'></a>
 		<span class='gridText'>Trace 2</span>
 	</div>
 
 	<div class="gridItem">											
-		<a href="https://itunes.apple.com/us/app/gomi/id325288931" target="_blank">
-		<img class='gridImage' src="Images/gomiGameIcon.png"></a>
+		<a href="https://itunes.apple.com/us/app/gomi/id325288931" target="_blank" rel="noopener">
+		<img class='gridImage' src="Images/gomiGameIcon.png" alt='Gomi'></a>
 		<span class='gridText'>Gomi</span>
 	</div>
 
 	<div class="gridItem">
-		<a href="https://itunes.apple.com/us/app/trace/id289446636" target="_blank">
-		<img class='gridImage' src="Images/trace1GameIcon.png"></a>
+		<a href="https://itunes.apple.com/us/app/trace/id289446636" target="_blank" rel="noopener">
+		<img class='gridImage' src="Images/trace1GameIcon.png" alt='Trace'></a>
 		<span class='gridText'>Trace</span>
 	</div>
 
