becd0bb96e06f4cb4a2d5943e95fd0f818a2b31d
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-23 11:35:15 -0700

    Bugfixes for Google Lighthouse

--- music.php
+++ music.php
@@ -12,79 +12,79 @@
 
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/transcend">
-		<img class='gridImage' src="Images/transcendBoxMini.png"></a>
+		<img class='gridImage' src="Images/transcendBoxMini.png" alt='Transcend'></a>
 		<span class='gridText'>Transcend</span>
 	</div>	
 	
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/rush150">
-		<img class='gridImage' src="Images/rush150BoxMini.jpg"></a>
+		<img class='gridImage' src="Images/rush150BoxMini.jpg" alt='Rush 150%'></a>
 		<span class='gridText'>Rush 150%</span>
 	</div>
 	
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/rushdetuned">
-		<img class='gridImageKongRectangle' src="Images/rushDetunedBoxMini.jpg"></a>
+		<img class='gridImageKongRectangle' src="Images/rushDetunedBoxMini.jpg" alt='Rush Detuned'></a>
 		<span class='gridText'>Rush Detuned</span>
 	</div>
 
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/trace2">
-		<img class='gridImage' src="Images/trace2BoxMini.png"></a>
+		<img class='gridImage' src="Images/trace2BoxMini.png" alt='Trace 2'></a>
 		<span class='gridText'>Trace 2</span>
 	</div>	
 	
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/trace15">
-		<img class='gridImage' src="Images/trace15BoxMini.png"></a>
+		<img class='gridImage' src="Images/trace15BoxMini.png" alt='Trace 1.5'></a>
 		<span class='gridText'>Trace 1.5</span>
 	</div>
 	
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/cloudpulse">
-		<img class='gridImage' width=175px; src="Images/cloudpulseBoxMini.png"></a>
+		<img class='gridImage' width=175px; src="Images/cloudpulseBoxMini.png" alt='Cloudpulse'></a>
 		<span class='gridText'>Cloudpulse</span>
 	</div>
 
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/youarelegend">
-		<img class='gridImage' width=175px; src="Images/legendBoxMini.png"></a>
+		<img class='gridImage' width=175px; src="Images/legendBoxMini.png" alt='You Are Legend'></a>
 		<span class='gridText'>You Are Legend</span>
 	</div>	
 	
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/pixelquartet">
-		<img class='gridImage' width=175px; src="Images/pixelquartetBoxMini.png"></a>
+		<img class='gridImage' width=175px; src="Images/pixelquartetBoxMini.png" alt='Pixel Quartet'></a>
 		<span class='gridText'>Pixel Quartet</span>
 	</div>
 	
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/sekaiju">
-		<img class='gridImage' width=175px; src="Images/sekaijuBox.png"></a>
+		<img class='gridImage' width=175px; src="Images/sekaijuBox.png" alt='Sekaiju'></a>
 		<span class='gridText'>Sekaiju</span>
 	</div>
 
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/gomi">
-		<img class='gridImage' src="Images/gomiBoxMini.png"></a>
+		<img class='gridImage' src="Images/gomiBoxMini.png" alt='Gomi'></a>
 		<span class='gridText'>Gomi</span>
 	</div>	
 	
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/trace">
-		<img class='gridImage' src="Images/traceBoxMini.png"></a>
+		<img class='gridImage' src="Images/traceBoxMini.png" alt='Trace'></a>
 		<span class='gridText'>Trace</span>
 	</div>
 	
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/random">
-		<img class='gridImage' width=175px; src="Images/randomBox.png"></a>
+		<img class='gridImage' width=175px; src="Images/randomBox.png" alt='Random Music'></a>
 		<span class='gridText'>Random Music</span>
 	</div>
 
 	<div class="gridItem">
 		<a href="https://www.taylorcalderone.com/classical">
-		<img class='gridImage' width=175px; src="Images/randomBox.png"></a>
+		<img class='gridImage' width=175px; src="Images/randomBox.png" alt='Classical Music'></a>
 		<span class='gridText'>Classical Music</span>
 	</div>
 
