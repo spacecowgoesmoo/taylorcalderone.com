2e5e048426444db3119c35e324e1600b8522f4ef
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-24 11:12:34 -0700

    IMG alt tags

--- sekaiju.php
+++ sekaiju.php
@@ -7,42 +7,42 @@
 	
 	<div class='musicPageCenteredPlaylist'>
 		<div class='musicPageRow'>
-			<img id='button1' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Town.mp3", this.id);'>
+			<img id='button1' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Town.mp3", this.id);'>
 			<div class='musicPageRowText'>Town - Adventurers Gather in a Faraway Land</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button2' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth1.mp3", this.id);'>
+			<img id='button2' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth1.mp3", this.id);'>
 			<div class='musicPageRowText'>Labyrinth I - Grove Under the Sapphire Mist</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button3' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth2.mp3", this.id);'>
+			<img id='button3' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth2.mp3", this.id);'>
 			<div class='musicPageRowText'>Labyrinth II - The Dark Violet Woodlands</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button4' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth3.mp3", this.id);'>
+			<img id='button4' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth3.mp3", this.id);'>
 			<div class='musicPageRowText'>Labyrinth III - Forgotten Blue Lakeshore</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button5' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth4.mp3", this.id);'>
+			<img id='button5' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth4.mp3", this.id);'>
 			<div class='musicPageRowText'>Labyrinth IV - Garden of Red Roses</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button6' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth5.mp3", this.id);'>
+			<img id='button6' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth5.mp3", this.id);'>
 			<div class='musicPageRowText'>Labyrinth V - The Stars Will Still Guide Us</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button7' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Credits.mp3", this.id);'>
+			<img id='button7' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Credits.mp3", this.id);'>
 			<div class='musicPageRowText'>Legends for a New Generation</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button8' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth6.mp3", this.id);'>
+			<img id='button8' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Sekaiju/Labyrinth6.mp3", this.id);'>
 			<div class='musicPageRowText'>Labyrinth VI - Depths of Eternal Silver</div>
 		</div>
 	</div>
@@ -54,18 +54,18 @@
 	<center>
 	<br><br><br><br><br>
 		Avaliable for download on						
-		<a href="https://spacecowgoesmoo.bandcamp.com/album/sekaiju" target="_blank"><img class='bandcampButton' src="Images/bandcamp2017.png"></a>
+		<a href="https://spacecowgoesmoo.bandcamp.com/album/sekaiju" target="_blank" rel='noopener'><img class='bandcampButton' alt='Bandcamp' src="Images/bandcamp2017.png"></a>
 
 
 
 
 	<br><br><br><br>
-		<img class='sekaijuImage' src="Images/Labyrinth1.jpg">
-		<img class='sekaijuImage' src="Images/Labyrinth2.jpg">
-		<img class='sekaijuImage' src="Images/Labyrinth3.jpg">
-		<img class='sekaijuImage' src="Images/Labyrinth4.jpg">
-		<img class='sekaijuImage' src="Images/Labyrinth5.jpg">
-		<img class='sekaijuImage' src="Images/Labyrinth6.jpg">
+		<img class='sekaijuImage' src="Images/Labyrinth1.jpg" alt='Light Cyan Forest'>
+		<img class='sekaijuImage' src="Images/Labyrinth2.jpg" alt='Dark Purple Forest'>
+		<img class='sekaijuImage' src="Images/Labyrinth3.jpg" alt='Dark Blue Lakeside'>
+		<img class='sekaijuImage' src="Images/Labyrinth4.jpg" alt='Misty Green Hills With Roses'>
+		<img class='sekaijuImage' src="Images/Labyrinth5.jpg" alt='Lone Tree and Starfield'>
+		<img class='sekaijuImage' src="Images/Labyrinth6.jpg" alt='Misty Silver Hills'>
 	<br>
 	<div class='tinyFont'>All images are from Google and are copyrighted by their respective owners.</div>
 </div>
