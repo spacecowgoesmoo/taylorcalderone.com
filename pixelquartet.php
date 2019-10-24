a73bfafb494a7b03015da34d1da91e08ba3743d5
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-24 11:12:10 -0700

    IMG alt tags

--- pixelquartet.php
+++ pixelquartet.php
@@ -6,168 +6,168 @@
 <div class='musicPageFormat'>
 	
 	<div class='musicPageLeft'>
-		<img class='musicPageAlbumCover' src="Images/pixelquartetBox.png">
+		<img class='musicPageAlbumCover' src="Images/pixelquartetBox.png" alt='Pixel Quartet Boxart'>
 	</div>
 
 
 	<div class='musicPageRight'>
 		<div class='musicPageRow'>
-			<img id='button1' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level3.mp3", this.id);'>
+			<img id='button1' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level3.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 3</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button2' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level4.mp3", this.id);'>
+			<img id='button2' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level4.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 4</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button3' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level5.mp3", this.id);'>
+			<img id='button3' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level5.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 5</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button4' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level6.mp3", this.id);'>
+			<img id='button4' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level6.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 6</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button5' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level7.mp3", this.id);'>
+			<img id='button5' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level7.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 7</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button6' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level8.mp3", this.id);'>
+			<img id='button6' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level8.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 8</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button7' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level9.mp3", this.id);'>
+			<img id='button7' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level9.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 9</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button8' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level10.mp3", this.id);'>
+			<img id='button8' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level10.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 10</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button9' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level11.mp3", this.id);'>
+			<img id='button9' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level11.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 11</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button10' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level12.mp3", this.id);'>
+			<img id='button10' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level12.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 12</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button11' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level13.mp3", this.id);'>
+			<img id='button11' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level13.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 13</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button12' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level14.mp3", this.id);'>
+			<img id='button12' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level14.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 14</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button13' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level15.mp3", this.id);'>
+			<img id='button13' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level15.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 15</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button14' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level16.mp3", this.id);'>
+			<img id='button14' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level16.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 16</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button15' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level17.mp3", this.id);'>
+			<img id='button15' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level17.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 17</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button16' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level18.mp3", this.id);'>
+			<img id='button16' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level18.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 18</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button17' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level19.mp3", this.id);'>
+			<img id='button17' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level19.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 19</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button18' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level20.mp3", this.id);'>
+			<img id='button18' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level20.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 20</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button19' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level21.mp3", this.id);'>
+			<img id='button19' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level21.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 21</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button20' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level22.mp3", this.id);'>
+			<img id='button20' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level22.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 22</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button21' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level23.mp3", this.id);'>
+			<img id='button21' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level23.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 23</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button22' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level24.mp3", this.id);'>
+			<img id='button22' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level24.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 24</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button23' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level25.mp3", this.id);'>
+			<img id='button23' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level25.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 25</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button24' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level26.mp3", this.id);'>
+			<img id='button24' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level26.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 26</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button25' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level27.mp3", this.id);'>
+			<img id='button25' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level27.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 27</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button26' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level28.mp3", this.id);'>
+			<img id='button26' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level28.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 28</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button27' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level29.mp3", this.id);'>
+			<img id='button27' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level29.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 29</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button28' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level30.mp3", this.id);'>
+			<img id='button28' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Level30.mp3", this.id);'>
 			<div class='musicPageRowText'>Level 30</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button29' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Extra1.mp3", this.id);'>
+			<img id='button29' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Extra1.mp3", this.id);'>
 			<div class='musicPageRowText'>Deleted Track 1</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button30' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Extra2.mp3", this.id);'>
+			<img id='button30' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Extra2.mp3", this.id);'>
 			<div class='musicPageRowText'>Deleted Track 2</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button31' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Easy.mp3", this.id);'>
+			<img id='button31' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/Easy.mp3", this.id);'>
 			<div class='musicPageRowText'>Beta Stage - Easy</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button32' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/IIDX.mp3", this.id);'>
+			<img id='button32' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/PixelQuartet/IIDX.mp3", this.id);'>
 			<div class='musicPageRowText'>Beta Stage - IIDX</div>
 		</div>
 	</div>	
