5a6ee9eb45fc5d66fcd4a34752f4bc2a8a44c8d1
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-24 11:20:49 -0700

    IMG alt tags

--- youarelegend.php
+++ youarelegend.php
@@ -6,7 +6,7 @@
 <div class='musicPageFormat'>
 	
 	<div class='musicPageLeft'>
-		<img class='musicPageAlbumCover' src="Images/legendBox.png">
+		<img class='musicPageAlbumCover' src="Images/legendBox.png" alt='Legend Boxart'>
 	</div>
 
 
@@ -14,22 +14,22 @@
 
 	<div class='musicPageRight'>
 		<div class='musicPageRow'>
-			<img id='button1' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/YouAreLegend/TitleScreen.mp3", this.id);'>
+			<img id='button1' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/YouAreLegend/TitleScreen.mp3", this.id);'>
 			<div class='musicPageRowText'>An Invisible Legend</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button2' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/YouAreLegend/Ambience.mp3", this.id);'>
+			<img id='button2' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/YouAreLegend/Ambience.mp3", this.id);'>
 			<div class='musicPageRowText'>Silence</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button3' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/YouAreLegend/Zombies.mp3", this.id);'>
+			<img id='button3' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/YouAreLegend/Zombies.mp3", this.id);'>
 			<div class='musicPageRowText'>Conflict</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button4' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/YouAreLegend/DnR.mp3", this.id);'>
+			<img id='button4' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/YouAreLegend/DnR.mp3", this.id);'>
 			<div class='musicPageRowText'>Death & Resurrection</div>
 		</div>
 
@@ -40,6 +40,6 @@
 
 	<div class='musicPageFooter'>
 		Avaliable for download on						
-		<a href="https://spacecowgoesmoo.bandcamp.com/album/you-are-legend" target="_blank"><img class='bandcampButton' src="Images/bandcamp2017.png"></a>
+		<a href="https://spacecowgoesmoo.bandcamp.com/album/you-are-legend" target="_blank" rel='noopener'><img class='bandcampButton' alt='Bandcamp' src="Images/bandcamp2017.png"></a>
 	</div>
 </div>
