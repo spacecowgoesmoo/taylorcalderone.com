254f192a71f41861087e32f676c80c0643c5be08
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-24 11:20:30 -0700

    IMG alt tags

--- cloudpulse.php
+++ cloudpulse.php
@@ -6,7 +6,7 @@
 <div class='musicPageFormat'>
 	
 	<div class='musicPageLeft'>
-		<img class='musicPageAlbumCover' src="Images/cloudpulseBox.png">
+		<img class='musicPageAlbumCover' src="Images/cloudpulseBox.png" alt='Cloudpulse Boxart'>
 	</div>
 
 
@@ -14,42 +14,42 @@
 
 	<div class='musicPageRight'>
 		<div class='musicPageRow'>
-			<img id='button1' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/Sunlight.mp3", this.id);'>
+			<img id='button1' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/Sunlight.mp3", this.id);'>
 			<div class='musicPageRowText'>Sunlight</div>
 		</div>
 	
 		<div class='musicPageRow'>
-			<img id='button2' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/CloudpulseOne.mp3", this.id);'>
+			<img id='button2' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/CloudpulseOne.mp3", this.id);'>
 			<div class='musicPageRowText'>Cloudpulse One</div>
 		</div>
 	
 		<div class='musicPageRow'>
-			<img id='button3' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/FirstDimension.mp3", this.id);'>
+			<img id='button3' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/FirstDimension.mp3", this.id);'>
 			<div class='musicPageRowText'>First Dimension</div>
 		</div>
 	
 		<div class='musicPageRow'>
-			<img id='button4' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/StatesofExistence.mp3", this.id);'>
+			<img id='button4' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/StatesofExistence.mp3", this.id);'>
 			<div class='musicPageRowText'>States of Existence</div>
 		</div>
 	
 		<div class='musicPageRow'>
-			<img id='button5' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/ADreamofCivilization.mp3", this.id);'>
+			<img id='button5' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/ADreamofCivilization.mp3", this.id);'>
 			<div class='musicPageRowText'>A Dream of Civilization</div>
 		</div>
 	
 		<div class='musicPageRow'>
-			<img id='button6' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/GrandUnifyingTheory.mp3", this.id);'>
+			<img id='button6' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/GrandUnifyingTheory.mp3", this.id);'>
 			<div class='musicPageRowText'>Grand Unifying Theory</div>
 		</div>
 	
 		<div class='musicPageRow'>
-			<img id='button7' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/Infinity.mp3", this.id);'>
+			<img id='button7' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/Infinity.mp3", this.id);'>
 			<div class='musicPageRowText'>Infinity</div>
 		</div>
 	
 		<div class='musicPageRow'>
-			<img id='button8' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/Faith.mp3", this.id);'>
+			<img id='button8' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Cloudpulse/Faith.mp3", this.id);'>
 			<div class='musicPageRowDoubleText'>Where Others Can See Only Through Faith</div>
 		</div>
 	</div>
@@ -59,6 +59,6 @@
 		
 	<div class='musicPageFooter'>
 		Avaliable for download on						
-		<a href="https://spacecowgoesmoo.bandcamp.com/album/cloudpulse" target="_blank"><img class='bandcampButton' src="Images/bandcamp2017.png"></a>
+		<a href="https://spacecowgoesmoo.bandcamp.com/album/cloudpulse" target="_blank" rel='noopener'><img class='bandcampButton' alt='Bandcamp' src="Images/bandcamp2017.png"></a>
 	</div>
 </div>
