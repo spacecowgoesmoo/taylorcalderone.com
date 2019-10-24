356f4ef7084a4695ea47df4807c16f6613247580
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-24 11:31:06 -0700

    IMG alt tags

--- rush150.php
+++ rush150.php
@@ -6,7 +6,7 @@
 <div class='musicPageFormat'>
 
 	<div class='musicPageLeft'>
-		<img class='musicPageAlbumCover' src="Images/rush150Box.jpg">
+		<img class='musicPageAlbumCover' src="Images/rush150Box.jpg" alt='Rush 150% Boxart'>
 	</div>
 
 
@@ -14,62 +14,62 @@
 
 	<div class='musicPageRight'>
 		<div class='musicPageRow'>
-			<img id='button1' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/l.mp3", this.id);'>
+			<img id='button1' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/l.mp3", this.id);'>
 			<div class='musicPageRowText'>Limbo</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button2' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/gnf.mp3", this.id);'>
+			<img id='button2' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/gnf.mp3", this.id);'>
 			<div class='musicPageRowText'>Good News First</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button3' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/v.mp3", this.id);'>
+			<img id='button3' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/v.mp3", this.id);'>
 			<div class='musicPageRowText'>Virtuality</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button4' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/sm.mp3", this.id);'>
+			<img id='button4' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/sm.mp3", this.id);'>
 			<div class='musicPageRowText'>Sweet Miracle</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button5' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/tmmb.mp3", this.id);'>
+			<img id='button5' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/tmmb.mp3", this.id);'>
 			<div class='musicPageRowText'>The Main Monkey Business</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button6' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/tam.mp3", this.id);'>
+			<img id='button6' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/tam.mp3", this.id);'>
 			<div class='musicPageRowText'>Time and Motion</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button7' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/mn.mp3", this.id);'>
+			<img id='button7' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/mn.mp3", this.id);'>
 			<div class='musicPageRowText'>Malignant Narcissism</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button8' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/bu2b.mp3", this.id);'>
+			<img id='button8' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/bu2b.mp3", this.id);'>
 			<div class='musicPageRowText'>BU2B</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button9' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/he.mp3", this.id);'>
+			<img id='button9' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/he.mp3", this.id);'>
 			<div class='musicPageRowText'>Halo Effect</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button10' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/hf.mp3", this.id);'>
+			<img id='button10' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/hf.mp3", this.id);'>
 			<div class='musicPageRowText'>Headlong Flight</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button11' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/wtw.mp3", this.id);'>
+			<img id='button11' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/wtw.mp3", this.id);'>
 			<div class='musicPageRowText'>Wish Them Well</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button12' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/tg.mp3", this.id);'>
+			<img id='button12' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Rush150/tg.mp3", this.id);'>
 			<div class='musicPageRowText'>The Garden</div>
 		</div>
 
