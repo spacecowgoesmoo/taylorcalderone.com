7f4d1e1bdd6bd76edb28931f181ed230f95a9d79
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-24 11:33:06 -0700

    IMG alt tags

--- transcend.php
+++ transcend.php
@@ -6,7 +6,7 @@
 <div class='musicPageFormat'>
 
 	<div class='musicPageLeft'>
-		<img class='musicPageAlbumCover' src="Images/transcendBox.png">
+		<img class='musicPageAlbumCover' src="Images/transcendBox.png" alt='Transcend Boxart'>
 	</div>
 
 
@@ -14,87 +14,87 @@
 
 	<div class='musicPageRight'>
 		<div class='musicPageRow'>
-			<img id='button1' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/bota.mp3", this.id);'>
+			<img id='button1' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/bota.mp3", this.id);'>
 			<div class='musicPageRowText'>Bring On The Abstraction</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button2' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/es.mp3", this.id);'>
+			<img id='button2' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/es.mp3", this.id);'>
 			<div class='musicPageRowText'>Electric Sunrise</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button3' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/bluep.mp3", this.id);'>
+			<img id='button3' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/bluep.mp3", this.id);'>
 			<div class='musicPageRowText'>Blue Passion</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button4' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/chs.mp3", this.id);'>
+			<img id='button4' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/chs.mp3", this.id);'>
 			<div class='musicPageRowText'>Cold Hearted Spotlight</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button5' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/rw.mp3", this.id);'>
+			<img id='button5' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/rw.mp3", this.id);'>
 			<div class='musicPageRowText'>Red Whisper</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button6' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/cute.mp3", this.id);'>
+			<img id='button6' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/cute.mp3", this.id);'>
 			<div class='musicPageRowText'>Crank Up The Ecstasy</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button7' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/ymf.mp3", this.id);'>
+			<img id='button7' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/ymf.mp3", this.id);'>
 			<div class='musicPageRowText'>You're My Fire</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button8' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/cstp.mp3", this.id);'>
+			<img id='button8' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/cstp.mp3", this.id);'>
 			<div class='musicPageRowText'>Can't Stop The Punk</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button9' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/licfy.mp3", this.id);'>
+			<img id='button9' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/licfy.mp3", this.id);'>
 			<div class='musicPageRowText'>Lightning is Comin For You</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button10' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/ffc.mp3", this.id);'>
+			<img id='button10' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/ffc.mp3", this.id);'>
 			<div class='musicPageRowText'>Far From Consequence</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button11' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/cio.mp3", this.id);'>
+			<img id='button11' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/cio.mp3", this.id);'>
 			<div class='musicPageRowText'>Chaos is Okay</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button12' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/pr.mp3", this.id);'>
+			<img id='button12' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/pr.mp3", this.id);'>
 			<div class='musicPageRowText'>Perfect Repetition</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button13' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/beatp.mp3", this.id);'>
+			<img id='button13' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/beatp.mp3", this.id);'>
 			<div class='musicPageRowText'>Beat Paradise</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button14' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/tt.mp3", this.id);'>
+			<img id='button14' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/tt.mp3", this.id);'>
 			<div class='musicPageRowText'>trace.transcend</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button15' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/se.mp3", this.id);'>
+			<img id='button15' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/se.mp3", this.id);'>
 			<div class='musicPageRowText'>Supersonic Eternity</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button16' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/fs.mp3", this.id);'>
+			<img id='button16' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/fs.mp3", this.id);'>
 			<div class='musicPageRowText'>Fading Space</div>
 		</div>
 
 		<div class='musicPageRow'>
-			<img id='button17' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/sn.mp3", this.id);'>
+			<img id='button17' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Transcend/sn.mp3", this.id);'>
 			<div class='musicPageRowText'>Stray Nightmare</div>
 		</div>
 
@@ -105,6 +105,6 @@
 
 	<div class='musicPageFooter'>
 		Avaliable for download on
-		<a href="https://spacecowgoesmoo.bandcamp.com/album/transcend" target="_blank"><img class='bandcampButton' src="Images/bandcamp2017.png"></a>
+		<a href="https://spacecowgoesmoo.bandcamp.com/album/transcend" target="_blank" rel='noopener'><img class='bandcampButton' alt='Bandcamp' src="Images/bandcamp2017.png"></a>
 	</div>
 </div>
