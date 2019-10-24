c834c1c911ab50f6da3a994205f7e277838d2c77
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-24 11:04:05 -0700

    IMG alt tags

--- trace.php
+++ trace.php
@@ -6,7 +6,7 @@
 <div class='musicPageFormat'>
 	
 	<div class='musicPageLeft'>
-		<img class='musicPageAlbumCover' src="Images/traceBox.png">
+		<img class='musicPageAlbumCover' src="Images/traceBox.png" alt='Trace Boxart'>
 	</div>
 
 
@@ -14,37 +14,37 @@
 
 	<div class='musicPageRight'>
 		<div class='musicPageRow'>
-			<img id='button1' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Trace.mp3", this.id);'>
+			<img id='button1' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Trace.mp3", this.id);'>
 			<div class='musicPageRowText'>Trace</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button2' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Paint.mp3", this.id);'>
+			<img id='button2' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Paint.mp3", this.id);'>
 			<div class='musicPageRowText'>Paint</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button3' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Water.mp3", this.id);'>
+			<img id='button3' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Water.mp3", this.id);'>
 			<div class='musicPageRowText'>Water</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button4' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Plant.mp3", this.id);'>
+			<img id='button4' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Plant.mp3", this.id);'>
 			<div class='musicPageRowText'>Plant</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button5' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Chalk.mp3", this.id);'>
+			<img id='button5' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Chalk.mp3", this.id);'>
 			<div class='musicPageRowText'>Chalk</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button6' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Flame.mp3", this.id);'>
+			<img id='button6' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Flame.mp3", this.id);'>
 			<div class='musicPageRowText'>Flame</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button7' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Space.mp3", this.id);'>
+			<img id='button7' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Trace/Space.mp3", this.id);'>
 			<div class='musicPageRowText'>Space</div>
 		</div>
 	</div>
@@ -54,6 +54,6 @@
 	
 	<div class='musicPageFooter'>
 		Avaliable for download on						
-		<a href="https://spacecowgoesmoo.bandcamp.com/album/trace" target="_blank"><img class='bandcampButton' src="Images/bandcamp2017.png"></a>
+		<a href="https://spacecowgoesmoo.bandcamp.com/album/trace" target="_blank" rel="noopener"><img class='bandcampButton' alt='Bandcamp' src="Images/bandcamp2017.png"></a>
 	</div>
 </div>
