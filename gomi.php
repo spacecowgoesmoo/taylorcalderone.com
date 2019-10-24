15fd83a52049e37ad56b03e907c9e07a8068543e
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-24 11:04:25 -0700

    IMG alt tags

--- gomi.php
+++ gomi.php
@@ -6,7 +6,7 @@
 <div class='musicPageFormat'>
 	
 	<div class='musicPageLeft'>
-		<img class='musicPageAlbumCover' src="Images/gomiBox.png">
+		<img class='musicPageAlbumCover' src="Images/gomiBox.png" alt='Gomi Boxart'>
 	</div>
 
 
@@ -14,87 +14,87 @@
 
 	<div class='musicPageRight'>
 		<div class='musicPageRow'>
-			<img id='button1' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/TitleScreen.mp3", this.id);'>
+			<img id='button1' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/TitleScreen.mp3", this.id);'>
 			<div class='musicPageRowText'>Rainbow Fields</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button2' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Metropolis.mp3", this.id);'>
+			<img id='button2' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Metropolis.mp3", this.id);'>
 			<div class='musicPageRowText'>Metropolis</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button3' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Camp.mp3", this.id);'>
+			<img id='button3' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Camp.mp3", this.id);'>
 			<div class='musicPageRowText'>Sierra</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button4' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/BossBattle1.mp3", this.id);'>
+			<img id='button4' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/BossBattle1.mp3", this.id);'>
 			<div class='musicPageRowText'>Pollution!</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button5' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/BabySeals.mp3", this.id);'>
+			<img id='button5' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/BabySeals.mp3", this.id);'>
 			<div class='musicPageRowText'>Frozen Wonderland</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button6' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/MinigameJ1.mp3", this.id);'>
+			<img id='button6' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/MinigameJ1.mp3", this.id);'>
 			<div class='musicPageRowText'>Afterdark Arcade</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button7' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Resort.mp3", this.id);'>
+			<img id='button7' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Resort.mp3", this.id);'>
 			<div class='musicPageRowText'>Seaside Park</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button8' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/BossBattle2.mp3", this.id);'>
+			<img id='button8' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/BossBattle2.mp3", this.id);'>
 			<div class='musicPageRowText'>Forest Fire!</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button9' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Rainforest.mp3", this.id);'>
+			<img id='button9' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Rainforest.mp3", this.id);'>
 			<div class='musicPageRowText'>Endangered Species</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button10' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Moon.mp3", this.id);'>
+			<img id='button10' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Moon.mp3", this.id);'>
 			<div class='musicPageRowText'>Space Cow</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button11' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/BossBattle3.mp3", this.id);'>
+			<img id='button11' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/BossBattle3.mp3", this.id);'>
 			<div class='musicPageRowText'>Oil Spill!</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button12' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Factory.mp3", this.id);'>
+			<img id='button12' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Factory.mp3", this.id);'>
 			<div class='musicPageRowText'>Miracle of Science</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button13' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/MinigameJ2.mp3", this.id);'>
+			<img id='button13' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/MinigameJ2.mp3", this.id);'>
 			<div class='musicPageRowText'>Starlight</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button14' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Homeworld.mp3", this.id);'>
+			<img id='button14' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Homeworld.mp3", this.id);'>
 			<div class='musicPageRowText'>Desolation</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button15' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Homeworld2.mp3", this.id);'>
+			<img id='button15' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Homeworld2.mp3", this.id);'>
 			<div class='musicPageRowText'>Shadows</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button16' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/FinalBoss.mp3", this.id);'>
+			<img id='button16' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/FinalBoss.mp3", this.id);'>
 			<div class='musicPageRowText'>Of Our Own Creation</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button17' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Credits.mp3", this.id);'>
+			<img id='button17' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Gomi/Credits.mp3", this.id);'>
 			<div class='musicPageRowText'>Beautiful World</div>
 		</div>
 	</div>
@@ -104,7 +104,7 @@
 	
 	<div class='musicPageFooter smallFont'>
 		"...a stunning soundtrack that to our ears sounded similar to the work of synthesizer maestro Vangelis." - 
-		<a href="https://web.archive.org/web/20190211195412/http://www.slidetoplay.com/gomi-review/" target="_blank">Slide to Play</a>
+		<a href="https://web.archive.org/web/20190211195412/http://www.slidetoplay.com/gomi-review/" target="_blank" rel="noopener">Slide to Play</a>
 	</div>
 
 
@@ -112,6 +112,6 @@
 	
 	<div class='musicPageFooter'>		
 		Avaliable for download on						
-		<a href="https://spacecowgoesmoo.bandcamp.com/album/gomi" target="_blank"><img class='bandcampButton' src="Images/bandcamp2017.png"></a>
+		<a href="https://spacecowgoesmoo.bandcamp.com/album/gomi" target="_blank" rel="noopener"><img class='bandcampButton' alt='Bandcamp' src="Images/bandcamp2017.png"></a>
 	</div>
 </div>
