744ae0c8912a49d4cf5d410b58c2118465381548
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-10-24 10:53:10 -0700

    IMG alt tags

--- classical.php
+++ classical.php
@@ -6,7 +6,7 @@
 <div class='musicPageFormat'>
 	
 	<div class='musicPageLeft'>
-		<img class='musicPageAlbumCover' src="Images/randomBox.png">
+		<img class='musicPageAlbumCover' src="Images/randomBox.png" alt='Classical Music Boxart'>
 	</div>
 
 
@@ -14,32 +14,32 @@
 
 	<div class='musicPageRight'>
 		<div class='musicPageRow'>
-			<img id='button1' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/Armageddon2.mp3", this.id);'>
+			<img id='button1' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/Armageddon2.mp3", this.id);'>
 			<div class='musicPageRowText'>Armageddon 2011</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button2' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/Stream.mp3", this.id);'>
+			<img id='button2' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/Stream.mp3", this.id);'>
 			<div class='musicPageRowText'>Stream</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button3' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/Superstructure.mp3", this.id);'>
+			<img id='button3' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/Superstructure.mp3", this.id);'>
 			<div class='musicPageRowText'>Superstructure</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button4' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/3671245.mp3", this.id);'>
+			<img id='button4' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/3671245.mp3", this.id);'>
 			<div class='musicPageRowText'>367.1245</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button5' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/Interval.mp3", this.id);'>
+			<img id='button5' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/Interval.mp3", this.id);'>
 			<div class='musicPageRowText'>Interval</div>
 		</div>
 		
 		<div class='musicPageRow'>
-			<img id='button6' class='audioButton' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/Armageddon.mp3", this.id);'>
+			<img id='button6' class='audioButton' alt='Play Button' onclick='playAudio("https://www.taylorcalderone.com/audioFiles/Classical/Armageddon.mp3", this.id);'>
 			<div class='musicPageRowText'>Armageddon 2008</div>
 		</div>
 	</div>
