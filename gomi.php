53f3435bb10a66ea098a7449e904b34c4ea09f34
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-12-30 16:14:04 -0800

    Readded footer

--- gomi.php
+++ gomi.php
@@ -115,3 +115,5 @@
 		<a href="https://spacecowgoesmoo.bandcamp.com/album/gomi" target="_blank" rel="noopener"><img class='bandcampButton' alt='Bandcamp' src="Images/bandcamp2017.png"></a>
 	</div>
 </div>
+
+<?php include('footer.php'); ?>
