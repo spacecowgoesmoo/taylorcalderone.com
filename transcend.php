642f5c2fa5d39d48a3079b1ca39a894503e60b62
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-12-30 16:18:03 -0800

    Readded footer

--- transcend.php
+++ transcend.php
@@ -108,3 +108,5 @@
 		<a href="https://spacecowgoesmoo.bandcamp.com/album/transcend" target="_blank" rel='noopener'><img class='bandcampButton' alt='Bandcamp' src="Images/bandcamp2017.png"></a>
 	</div>
 </div>
+
+<?php include('footer.php'); ?>
