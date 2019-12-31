9aa637248b10432caa49058f4e86873d85bcdd73
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-12-30 16:17:22 -0800

    Readded footer

--- trace2.php
+++ trace2.php
@@ -68,3 +68,5 @@
 		<a href="https://spacecowgoesmoo.bandcamp.com/album/trace-2" target="_blank" rel='noopener'><img class='bandcampButton' alt='Bandcamp' src="Images/bandcamp2017.png"></a>
 	</div>
 </div>
+
+<?php include('footer.php'); ?>
