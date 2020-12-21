3fb8ba2408b7bc07618592f952100c984a5a5708
Taylor Calderone <spacecowgoesmoo@gmail.com>
2020-12-20 16:52:20 -0800

    Updated play counts

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 	<p>I was a lead developer for a <a href='http://www.bovinedragonsoftware.com' target='_blank' rel='noopener'>startup</a> that created <a href ="https://toucharcade.com/2013/09/30/trace-2-review-years-ahead-of-the-first-game/" target='_blank' rel="noopener">three</a> <a href ="https://web.archive.org/web/20190211195412/http://www.slidetoplay.com/gomi-review/" target='_blank' rel="noopener">awesome</a> <a href='https://toucharcade.com/2008/10/08/spacecowmusics-trace-finally-hits-the-app-store/' target='_blank' rel='noopener'>games</a> during the early years of the iOS App Store. Our most successful product was installed over 4.8 million times and was showcased for years in Apple’s internal developer site as the definitive free iOS game.</p>
 
-	<p>I'm also experienced in HTML5 game development and web / digital audio. My background here includes two more online games with 126,000 and 18,000 plays, and I co-managed the IT web development team during my time working at a highly-ranked STEM college.</p>
+	<p>I'm also experienced in HTML5 game development and web / digital audio. My background here includes two more online games with 127,000 and 22,000 plays, and I co-managed the IT web development team during my time working at a highly-ranked STEM college.</p>
 
 	<br><br>email - spacecowgoesmoo@gmail.com
 	<br>twitter - <a href="https://twitter.com/spacecowgoesmoo" target='_blank' rel="noopener">@spacecowgoesmoo</a>
