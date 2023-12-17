32b647acfd183ffb7b9c2f2e067c3e5ec1696476
Taylor Calderone <spacecowgoesmoo@gmail.com>
2023-12-17 15:07:37 -0800

    Rewrote all the main text

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
 
 
 <div class='defaultPageFormat'>
-	My name is Taylor Calderone and I'm a Game Developer / Full Stack Developer / Music Producer living in the Los Angeles area.
+	I'm a Full Stack Developer / Game Developer / Music Producer in the Los Angeles area. Right now I'm working at Microsoft in the group that maintains <a href='https://zone.msn.com/en-us/home' target='_blank' rel='noopener'>zone.msn.com</a>.
 
-	<p>I was a lead developer for a <a href='http://www.bovinedragonsoftware.com' target='_blank' rel='noopener'>startup</a> that created <a href ="https://toucharcade.com/2013/09/30/trace-2-review-years-ahead-of-the-first-game/" target='_blank' rel="noopener">three</a> <a href ="https://web.archive.org/web/20190211195412/http://www.slidetoplay.com/gomi-review/" target='_blank' rel="noopener">awesome</a> <a href='https://toucharcade.com/2008/10/08/spacecowmusics-trace-finally-hits-the-app-store/' target='_blank' rel='noopener'>games</a> during the early years of the iOS App Store. Our most successful product was installed over 4.8 million times and was showcased for years in Apple’s internal developer site as the definitive free iOS game.</p>
+	<p>Previously, I worked at <a href='https://pop.in/' target='_blank' rel='noopener'>Pop.in</a> and helped create a new internal game platform for 2021’s app relaunch. Our team developed six online multiplayer games with videos livestreamed from players. I've also created two HTML5 games with 138,000 and 29,000 plays, and I co-managed the IT web development team during my time working at a highly-ranked STEM college.</p>
 
-	<p>I'm also experienced in HTML5 game development and web / digital audio. My background here includes two more online games with 138,000 and 29,000 plays, and I co-managed the IT web development team during my time working at a highly-ranked STEM college.</p>
+	<p>During the early years of the iOS App Store I was a lead developer for a <a href='https://www.bovinedragonsoftware.com' target='_blank' rel='noopener'>startup</a> that created <a href ="https://toucharcade.com/2013/09/30/trace-2-review-years-ahead-of-the-first-game/" target='_blank' rel="noopener">three</a> <a href ="https://web.archive.org/web/20190211195412/http://www.slidetoplay.com/gomi-review/" target='_blank' rel="noopener">awesome</a> <a href='https://toucharcade.com/2008/10/08/spacecowmusics-trace-finally-hits-the-app-store/' target='_blank' rel='noopener'>games</a>. Our most successful product was installed over 4.8 million times and was showcased for years in Apple’s internal developer site as the definitive free iOS game.</p>
+
+	<p>And I'm an audio engineer with a Bachelor's Degree in composing electronic music. This is just a hobby now, but I'm a perfect fit if you have any plans for building something with a sound/music focus!</p>
 
 
 	<br><br>email - tcalderone85@gmail.com
